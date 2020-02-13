09081e9039d991a107fafcb6cff3079ce9af481d
Treinamento <Treinamento@RD723937.raiadrogasil.com>
2020-02-13 12:00:22 -0300

    alterando voo do pato

--- Pato.java
+++ Pato.java
@@ -9,7 +9,13 @@ public class Pato implements Voador {
 
     @Override
     public void voar() {
-       energia -= 5 ;
+        if(energia < 5){
+            System.out.println("Não posso voar nao tenho energia -_-");
+        }else{
+            energia -= 5 ;
+        }
+
+
         System.out.println("Estou voando como um pato");
         System.out.println("Energia: " + energia );
     }
